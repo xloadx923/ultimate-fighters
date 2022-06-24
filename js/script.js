1057914ec171b7634c5257153f1f48aae65c683f
Yann Guichard <xloadx923@msn.com>
2022-06-24 13:45:13 +0200

    Integrate font-awesome.min.css

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
 
 const heroes = [];
+
 async function waitingForResponse() {
     const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
     const todoList = await response.json();
 
     displayHeroes(todoList);
 }
+
+function getRandomNumber(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function changeFighter(){
+
+    document.getElementById('fighter1').addEventListener('change',function(){
+        if(this.checked) document.getElementById('fighter2').checked = false;
+    });
+    document.getElementById('fighter2').addEventListener('change',function(){
+        if(this.checked) document.getElementById('fighter1').checked = false;
+    });
+
+}
+
+function closeDetails(){
+    document.querySelector('.allDetails').addEventListener('click',function(event){ this.classList.toggle('display'); })
+}
+
 function displayFighter(fighter,hero){
     document.querySelector(fighter).firstElementChild.innerText = hero.name;
     document.querySelector(fighter).style= `background: url(${hero.images.lg}) no-repeat;background-size: contain;`;
+    document.querySelector(fighter).addEventListener('click',function(){ document.querySelector('.allDetails').classList.toggle('display'); });
+    displayStats('.textDetails',hero);
 }
-function displayStats(nameDetail,hero){
 
-    document.querySelector(nameDetail).innerText = "";
+function displayStats(nameDetail,hero){
+    document.querySelector(nameDetail).innerHTML = '';
     const stats = document.createElement('div');
+    stats.innerHTML += (nameDetail=='textDetails') ? '<i class="fa fa-times fa-2x fa-fw crossClose"></i>' : '';
     stats.innerHTML += `<p>Life :${hero.powerstats.life}</p>`;
     stats.innerHTML += `<p>Shield :${hero.powerstats.shield}</p>`;
     stats.innerHTML += `<p>Intelligence :${hero.powerstats.intelligence}</p>`;
@@ -25,7 +51,6 @@ function displayStats(nameDetail,hero){
 }
 
 function displayHeroes(todoList){
-    // console.log(todoList[0]['name'],todoList[0]['appearance']['eyeColor']);
     todoList.forEach(hero => {
         // console.log(hero)
         hero.powerstats.life = 100;
@@ -43,6 +68,7 @@ function displayHeroes(todoList){
         document.querySelector('.heroes').appendChild(heroDetail);
 
         changeFighter();
+        closeDetails();
 
         heroDetail.addEventListener('click',function(event){
             if(document.getElementById('fighter1').checked){
@@ -62,19 +88,3 @@ function displayHeroes(todoList){
 // console.log(heroes)
 
 waitingForResponse();
-function changeFighter(){
-
-    document.getElementById('fighter1').addEventListener('change',function(){
-        if(this.checked) document.getElementById('fighter2').checked = false;
-    });
-    document.getElementById('fighter2').addEventListener('change',function(){
-        if(this.checked) document.getElementById('fighter1').checked = false;
-    });
-
-}
-
-function getRandomNumber(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
