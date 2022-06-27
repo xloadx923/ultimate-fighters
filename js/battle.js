929d210256731cb8680123d535dd5218357edbc3
Yann Guichard <xloadx923@msn.com>
2022-06-27 15:56:35 +0200

    Simplification function fight()

--- js/battle.js
+++ js/battle.js
@@ -16,42 +16,47 @@ function setRound(a,b,c,d){
     textCombat.innerHTML += '<p>'+resultRound+'</p>';
 }
 
-function reductTab(loser){
-    //heroes.splice(heroes.indexOf(loser),1);
-}
+// function reductTab(loser){
+//     heroes.splice(heroes.indexOf(loser),1);
+// }
 
 /*************** Launch rounds ********************************/
+let interval,attackerFight, defenserFight, attackerLife, defenserLife, winner, loser;
+
 function fight(heroes){
 
     attackerFight = heroes.fighter1.powerstats.intelligence + getRandomNumber(0, heroes.fighter1.powerstats.weapon);
     defenserFight = heroes.fighter2.powerstats.intelligence + getRandomNumber(0, heroes.fighter2.powerstats.shield);
+    attackerLife  = heroes.fighter1.powerstats.life;
+    defenserLife  = heroes.fighter2.powerstats.life;
 
-    if(attackerFight > defenserFight && defenserFight > 0)      { heroes.fighter2.powerstats.life -= attackerFight; winner = heroes.fighter1; loser = heroes.fighter2;reductTab(heroes.fighter2); }
-    else if(defenserFight > attackerFight && attackerFight > 0) { heroes.fighter1.powerstats.life -= defenserFight; winner = heroes.fighter2; loser = heroes.fighter1;reductTab(heroes.fighter1); }
+    if(attackerFight > defenserFight && defenserFight > 0)      { defenserLife -= attackerFight; winner = heroes.fighter1; loser = heroes.fighter2;/*reductTab(heroes.fighter2);*/ }
+    else if(defenserFight > attackerFight && attackerFight > 0) { attackerLife -= defenserFight; winner = heroes.fighter2; loser = heroes.fighter1;/*reductTab(heroes.fighter1);*/ }
 
-    if(heroes.fighter1.powerstats.life > 0 || heroes.fighter2.powerstats.life > 0){ setRound(heroes.fighter1.powerstats.life,attackerFight,heroes.fighter2.powerstats.life,defenserFight); }
+    if(attackerLife > 0 || defenserLife > 0){ setRound(attackerLife,attackerFight,defenserLife,defenserFight); }
 
-    if(heroes.fighter1.powerstats.life <= 0 && heroes.fighter1.powerstats.life !== heroes.fighter2.powerstats.life)      { setResultRound(winner.name, loser.name); }
-    else if(heroes.fighter2.powerstats.life <= 0 && heroes.fighter1.powerstats.life !== heroes.fighter2.powerstats.life) { setResultRound(winner.name, loser.name); }
-    else if(heroes.fighter1.powerstats.life === heroes.fighter2.powerstats.life) {
-        setRound(heroes.fighter1.powerstats.life,attackerFight,heroes.fighter2.powerstats.life,defenserFight);
+    if(attackerLife <= 0 && attackerLife !== defenserLife)      { setResultRound(winner.name, loser.name); }
+    else if(defenserLife <= 0 && attackerLife !== defenserLife) { setResultRound(winner.name, loser.name); }
+    else if(attackerLife === defenserLife) {
+        setRound(attackerLife,attackerFight,defenserLife,defenserFight);
         console.log("Exaequo !!!!!");
         textCombat.innerHTML += '<p style="width: 100%;color:yellow;">Exaequo !!!!!</p>';
     }
-    if(heroes.fighter1.powerstats.life <= 0 || heroes.fighter2.powerstats.life <= 0) clearInterval(interval);
+    if(attackerLife <= 0 || defenserLife <= 0){
+        clearInterval(interval);
+        localStorage.setItem('attacker','');
+        localStorage.setItem('defenser','');
+    }
 
 }
 
-let interval,attackerFight, defenserFight, winner, loser;
-
 function battle(hero)
 {
-    // let attackerFight, defenserFight, winner, loser;
 
-    if( hero.id === parseInt(localStorage.getItem("attacker"))){
+    if( hero.id === parseInt(localStorage.getItem("attacker")) ){
         heroes.fighter1 = hero;
     }
-    if( hero.id === parseInt(localStorage.getItem("defenser"))){
+    if( hero.id === parseInt(localStorage.getItem("defenser")) ){
 
         heroes.fighter2 = hero;
         console.log("Démarrage du combat entre " + heroes.fighter1.name + " et " +  heroes.fighter2.name);
